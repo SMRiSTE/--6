a44316fa0e1f109ee9599f95220ad84377c46570
Ш.М.Р. <s.m.r.00800ist.@gmail.com>
2024-03-17 12:51:59 +0300

    Initial commit

---
+++ 1/1/1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#define MODE  1
+
+#ifdef MODE
+
+int add(int a, int b) {
+	std::cout << "Результат сложения: " << a + b;
+	return 0;
+}
+
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+
+#if MODE == 0
+		std::cout << "Работаю в режиме тренировки";
+	
+
+# elif MODE == 1
+		std::cout << "Работаю в боевом режиме" << std::endl;
+		int num1, num2;
+		std::cout << "Введите num1: ";
+		std::cin >> num1;
+		std::cout << "Введите num2: ";
+		std::cin >> num2;
+
+		add(num1, num2);
+	
+
+#else 
+		std::cout << "Неизвестный режим. Завершение работы";
+	
+#endif
+}
+
+#endif
+
+
+
+
+
+
+#ifndef MODE
+#error "Определите МОDE"
+#endif
---
+++ 2/2/2.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#define SUM(a,b) ((a)-(b))
+int main()
+{
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUM(a, b) << std::endl;
+	std::cout << SUM(a, b) * c << std::endl;
+	std::cout << SUM(a, b + c) * c << std::endl;
+}
