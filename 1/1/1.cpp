5273c28b3fc5b4360d8f749092eb45b8b352ea9c
Ш.М.Р. <s.m.r.00800ist.@gmail.com>
2024-03-19 00:01:46 +0300

    Initial commit

--- 1/1/1.cpp
+++ 1/1/1.cpp
@@ -3,9 +3,8 @@
 
 #ifdef MODE
 
-int add(int a, int b) {
+void add(int a, int b) {
 	std::cout << "Результат сложения: " << a + b;
-	return a + b;
 }
 
 int main() {
@@ -13,23 +12,23 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 
 #if MODE == 0
-		std::cout << "Работаю в режиме тренировки";
-	
+	std::cout << "Работаю в режиме тренировки";
+
 
 # elif MODE == 1
-		std::cout << "Работаю в боевом режиме" << std::endl;
-		int num1, num2;
-		std::cout << "Введите num1: ";
-		std::cin >> num1;
-		std::cout << "Введите num2: ";
-		std::cin >> num2;
+	std::cout << "Работаю в боевом режиме" << std::endl;
+	int num1, num2;
+	std::cout << "Введите num1: ";
+	std::cin >> num1;
+	std::cout << "Введите num2: ";
+	std::cin >> num2;
 
-		add(num1, num2);
-	
+	add(num1, num2);
+
 
 #else 
-		std::cout << "Неизвестный режим. Завершение работы";
-	
+	std::cout << "Неизвестный режим. Завершение работы";
+
 #endif
 }
 
